b4f1b1851796340f3372bcf8b9c21146f8057a36
mariush2 <mariush2@github.com>
2020-04-09 15:29:28 +0200

    fix: table now works with the updated API from middleware

--- src/components/project/AnnotationGroupResults.vue
+++ src/components/project/AnnotationGroupResults.vue
@@ -4,18 +4,13 @@
     <div v-if="!loading" class="panel">
       <div class="panel-heading">
         <p>{{ group.groupName }} results</p>
+        <a ref="download" style="display: none"/>
+        <button class="button" @click="downloadCSV(group.groupName)"><i class="fas fa-file-export" style="margin-right:5px"/>Export CSV</button>
       </div>
       <div class="panel-block">
-        <template v-if="analyses" v-for="analysis in analyses">
-          <div v-if="analysis.status === 'success'" :key="analysis.analysisId" class="analysis-container">
-            <div class="analysis-header">
-              <h2>Analysis ID: <span>{{ analysis.analysisId }}</span></h2>
-              <a ref="download" style="display: none"/>
-              <button class="button" @click="downloadCSV(analysis.analysisId)"><i class="fas fa-file-export" style="margin-right:5px"/>Export CSV</button>
-            </div>
             <b-table
-              v-if="analysis.annotations"
-              :data="analysis.annotations"
+              v-if="group.annotations"
+              :data="group.annotations"
             >
               <template #default="{row: annotation}">
                 <template v-for="(result, index) in annotation.results">
@@ -25,39 +20,31 @@
                         <div :key="component.name">
                           <h3>{{ component.name }}</h3>
                           <p class="subcomponent" :key="component.name + subcomponent.name" v-for="subcomponent in component.components">
-                            {{ subcomponent.name }}: <span>{{ subcomponent.val.toFixed(2) }}</span>
+                            {{ subcomponent.name }}: <span>{{ subcomponent.val.toFixed(3) }}</span>
                           </p>
                         </div>
                       </template>
                     </div>
                   </b-table-column>
                 </template>
-                <b-table-column centered>
-                  <button class="button is-link" @click="openAnnotation(annotation.imageId, annotation.annotationId)">View annotation</button>
+                <b-table-column width="150" centered>
+                  <button class="button is-link" @click="openAnnotation(annotation.annotationId)">View annotation</button>
                 </b-table-column>
+              </template>
+              <template #empty>
+                <div class="content has-text-grey has-text-centered">
+                  <p>No results found</p>
+                </div>
               </template>
             </b-table>
           </div>
-          <div v-else-if="analysis.status === 'pending'" :key="analysis.id" class="analysis-container">
-            <h2>Analysis ID: <span>{{ analysis.analysisId }}</span></h2>
-            <div class="status-div">
-              <p class="status">Pending...</p>
-              <progress class="progress is-small is-primary" max="100" />
-            </div>
-          </div>
-          <div v-else :key="analysis.id" analysis.container>
-            <h2>Analysis ID: <span>{{ analysis.analysisId }}</span></h2>
-            <div class="status-div">
-              <p class="status failed"><i class="fas fa-exclamation-triangle"/>Failed</p>
-            </div>
-          </div>
-        </template>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
+import { Annotation } from 'cytomine-client';
+
 export default {
   name: 'AnnotationGroupResults',
   data() {
@@ -67,28 +54,21 @@ export default {
       loading: true,
     };
   },
+  computed: {
+    projectId() {
+      return this.$router.history.current.params.idProject;
+    },
+  },
   methods: {
     async getGroup() {
-      const getInfo = await fetch(`${this.$store.state.baseUrl}/analysisInformation?annotationGroupId=${this.$router.history.current.params.idGroup}`);
-      const response = await getInfo.json();
-      return response;
+      return await fetch(`${this.$store.state.baseUrl}/analysisResults?groupId=${this.$router.history.current.params.idGroup}`);
     },
-    async getResults(analyses) {
-      for (let analysis of analyses) {
-        if (analysis.status === 'success') {
-          const getResults = await fetch(`${this.$store.state.baseUrl}/analysisResults?analysisId=${analysis.analysisId}`);
-          const response = await getResults.json();
-          analysis.annotations = response.annotations;
-        }
-      }
-      return analyses;
-    },
-    openAnnotation(imageId, annotationId) {
+    async openAnnotation(annotationId) {
       const projectId = this.$router.history.current.params.idProject;
-      this.$router.push({ path: `/project/${projectId}/image/${imageId}/annotation/${annotationId}`});
+      const annotationObject = await Annotation.fetch(annotationId);
+      this.$router.push({ path: `/project/${projectId}/image/${annotationObject.image}/annotation/${annotationId}`});
     },
-    downloadCSV(analysisId) {
-      console.log(`downloading ${analysisId}`);
+    downloadCSV(name) {
       try {
         let csvContent = '';
         let a = this.$refs.download[0];
@@ -96,7 +76,7 @@ export default {
           blob = new Blob([csv], {type: 'data:application/octet-stream;base64'}),
           url = window.URL.createObjectURL(blob);
         a.href = url;
-        a.download = `${analysisId}-csv-export.csv`;
+        a.download = `${name}-csv-export.csv`;
         a.click();
         window.URL.revokeObjectURL(url);
         // Show snackbar
@@ -107,207 +87,16 @@ export default {
         this.$notify({type: 'error', text: 'An error occured when trying to export analysis results'});
       }
     },
-    shouldCheckProgress(analyses) {
-      return analyses.filter(analysis => analysis.status === 'pending').length > 0;
-    },
     async fetchData() {
-      this.group = await this.getGroup();
-      // let analyses = this.group.analyses;
-      // analyses = await this.getResults(analyses);
-      let analyses = [
-        {
-          analysisId: 'kasjdlhjsdjfghdkasd',
-          annotationGroupId: 'ajdhfgkjsdfhg',
-          annotations: [],
-          groupName: null,
-          status: 'failed',
-        },
-        {
-          analysisId: 'shjdfgsdk',
-          annotationGroupId: 'ajdhfgkjsdfhg',
-          annotations: [],
-          groupName: null,
-          status: 'pending',
-        },
-        {
-          analysisId: 'lskljdflk',
-          annotationGroupId: 'ajdhfgkjsdfhg',
-          annotations: [],
-          groupName: null,
-          status: 'success',
-        },
-      ];
-      for (let analysis of analyses) {
-        analysis.annotations = [
-          {
-            annotationId: '1032341',
-            imageId: '386531',
-            results: [
-              {
-                name: 'HE',
-                components: [
-                  {
-                    name: 'H',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: -0.20421988842343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.2842343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'E',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.241882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.242343536
-                      },
-                    ]
-                  }
-                ]
-              },
-              {
-                name: 'HSV',
-                components: [
-                  {
-                    name: 'H',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: -0.20421988842343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.2842343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'S',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.241882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.242343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'V',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.22343536
-                      },
-                    ]
-                  }
-                ]
-              }],
-          },
-          {
-            annotationId: '1032341',
-            imageId: '386531',
-            results: [
-              {
-                name: 'HE',
-                components: [
-                  {
-                    name: 'H',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: -0.20421988842343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.2842343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'E',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.241882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.242343536
-                      },
-                    ]
-                  }
-                ]
-              },
-              {
-                name: 'HSV',
-                components: [
-                  {
-                    name: 'H',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: -0.20421988842343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.2842343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'S',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.241882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.242343536
-                      },
-                    ]
-                  },
-                  {
-                    name: 'V',
-                    components: [
-                      {
-                        name: 'mean',
-                        val: 0.882343536
-                      },
-                      {
-                        name: 'std',
-                        val: 0.22343536
-                      },
-                    ]
-                  }
-                ]
-              }],
-          }
-        ];
-      }
-      if (this.shouldCheckProgress(analyses)) {
-        // Fetch analyses again in 10 seconds
+      let group = await this.getGroup();
+      if (group.status === 404) {
         setTimeout(() => {
           this.fetchData();
         }, 10000);
       }
-      this.analyses = analyses;
+      else {
+        this.group = await group.json();
+      }
     }
   },
   async created() {
@@ -318,9 +107,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .analysis-header {
+  .panel-heading {
     display: grid;
-    grid-template-columns: 1fr 0fr;
+    grid-template-columns: 1fr 1fr;
+
+    > button {
+      max-width: fit-content;
+      justify-self: flex-end;
+    }
   }
 
   h2 > span {
