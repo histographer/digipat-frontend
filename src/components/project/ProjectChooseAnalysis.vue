f81c8efd46fba41c05ddd66c786de3610acd4344
mariush2 <mariush2@github.com>
2020-05-02 10:45:06 +0200

    fix: changed createAnnotationGroup to GET from POST

--- src/components/project/ProjectChooseAnalysis.vue
+++ src/components/project/ProjectChooseAnalysis.vue
@@ -52,20 +52,7 @@ export default {
         // Needs to have a name, throw error
         this.$notify({type: 'error', text: 'You need to give the annotation group a name!'});
       }
-      const createResponse = await fetch(`${this.$store.state.baseUrl}/annotationGroup`, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': this.$store.state.baseUrl,
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer-when-downgrade', // no-referrer, *client
-        body: JSON.stringify(createGroupData) // body data type must match "Content-Type" header
-      });
+      const createResponse = await fetch(`${this.$store.state.baseUrl}/createAnnotationGroup?data=${btoa(JSON.stringify(createGroupData))}`);
       const createResponseData = await createResponse.json(); // parses JSON response into native JavaScript objects
       const startAnalysisData = {
         groupId: createResponseData.groupId,
@@ -79,10 +66,9 @@ export default {
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': this.$store.state.baseUrl,
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer-when-downgrade', // no-referrer, *client
+        referrerPolicy: 'no-referrer', // no-referrer, *client
         body: JSON.stringify(startAnalysisData) // body data type must match "Content-Type" header
       });
 
